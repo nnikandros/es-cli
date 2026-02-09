5bf841a56b84269c42b7e87d175457f50bfb468d
Nikitas Nikandros <Nikitas.NIKANDROS@ext.ec.europa.eu>
2026-02-09 10:52:09 +0000

    refactor

--- cmd/createIndexSubCommand.go
+++ cmd/createIndexSubCommand.go
@@ -36,6 +36,7 @@ es index create --directory ./elasticops/settings_mappings/test-index
 
 func addCreateFlags(create *cobra.Command) *cobra.Command {
 	create.Flags().StringP("directory", "d", "", "path to the directory where you have the mappings and settings")
+	create.Flags().StringP("path", "p", "", "path to the json file where you have the mappings and settings together")
 	return create
 
 }
@@ -43,7 +44,11 @@ func addCreateFlags(create *cobra.Command) *cobra.Command {
 func runCreateIndexCmdFunc(es *elasticsearch.TypedClient) RunEFunc {
 	return func(cmd *cobra.Command, args []string) error {
 
-		directory, err := cmd.Flags().GetString("directory")
+		mappings := types.TypeMapping{}
+		settings := types.IndexSettings{}
+
+		directory, _ := cmd.Flags().GetString("directory")
+		pathToJson, _ := cmd.Flags().GetString("path")
 
 		var indexName string
 
@@ -57,31 +62,29 @@ func runCreateIndexCmdFunc(es *elasticsearch.TypedClient) RunEFunc {
 
 		}
 
-		if err != nil {
-			return fmt.Errorf("at reading the value of the path flag %w", err)
-		}
+		if len(directory) != 0 {
+			absPathDirToMappingsAndSettings, _ := filepath.Abs(directory)
 
-		absPathDirToMappingsAndSettings, _ := filepath.Abs(directory)
-
-		_, err = os.Stat(absPathDirToMappingsAndSettings)
-		if err != nil {
-			if errors.Is(err, fs.ErrNotExist) {
-				return fmt.Errorf("the path %v does not exist, %w", directory, err)
+			_, err := os.Stat(absPathDirToMappingsAndSettings)
+			if err != nil {
+				if errors.Is(err, fs.ErrNotExist) {
+					return fmt.Errorf("the path %v does not exist, %w", directory, err)
+				}
+				return fmt.Errorf("stat of the %v returned an  error %w", directory, err)
 			}
-			return fmt.Errorf("stat of the %v returned an  error %w", directory, err)
+
+			pathToMappings := filepath.Join(absPathDirToMappingsAndSettings, "mappings.json")
+			pathToSettings := filepath.Join(absPathDirToMappingsAndSettings, "settings.json")
+
+			// to use serde.DecodeV2
+			mappings, err1 := serde.DecodeJsonFileToStruct[types.TypeMapping](pathToMappings)
+			settings, err2 := serde.DecodeJsonFileToStruct[types.IndexSettings](pathToSettings)
+
+			if err1 != nil || err2 != nil {
+				return fmt.Errorf("parsing the mappings or settings json files %w, %w", err1, err2)
+			}
+
 		}
-
-		pathToMappings := filepath.Join(absPathDirToMappingsAndSettings, "mappings.json")
-		pathToSettings := filepath.Join(absPathDirToMappingsAndSettings, "settings.json")
-
-		// to use serde.DecodeV2
-		mappings, err1 := serde.DecodeJsonFileToStruct[types.TypeMapping](pathToMappings)
-		settings, err2 := serde.DecodeJsonFileToStruct[types.IndexSettings](pathToSettings)
-
-		if err1 != nil || err2 != nil {
-			return fmt.Errorf("parsing the mappings or settings json files %w, %w", err1, err2)
-		}
-
 		r, err := createIndex(es, indexName, mappings, settings)
 		if err != nil {
 			return fmt.Errorf("at creating index: %w", err)
@@ -108,9 +111,45 @@ func createIndex(es *elasticsearch.TypedClient, indexName string, mappings types
 	return r, nil
 }
 
-// func MultipeErrorHandler(errors ...int) error {
-// 	fmt.Println(nums)
+type MappingsAndSettings struct {
+	Mappings types.TypeMapping   `json:"mappings"`
+	Settings types.IndexSettings `json:"settings"`
+}
 
-// 	return nil
+func handleDirCase(directory string) (MappingsAndSettings, error) {
+	absPathDirToMappingsAndSettings, _ := filepath.Abs(directory)
 
-// }
+	_, err := os.Stat(absPathDirToMappingsAndSettings)
+	if err != nil {
+		if errors.Is(err, fs.ErrNotExist) {
+			return MappingsAndSettings{}, fmt.Errorf("the path %v does not exist, %w", directory, err)
+		}
+		return MappingsAndSettings{}, fmt.Errorf("stat of the %v returned an  error %w", directory, err)
+	}
+
+	pathToMappings := filepath.Join(absPathDirToMappingsAndSettings, "mappings.json")
+	pathToSettings := filepath.Join(absPathDirToMappingsAndSettings, "settings.json")
+
+	// to use serde.DecodeV2
+	mappings, err1 := serde.DecodeJsonFileToStruct[types.TypeMapping](pathToMappings)
+	settings, err2 := serde.DecodeJsonFileToStruct[types.IndexSettings](pathToSettings)
+
+	if err1 != nil || err2 != nil {
+		return MappingsAndSettings{}, fmt.Errorf("parsing the mappings or settings json files %w, %w", err1, err2)
+	}
+
+	return MappingsAndSettings{mappings, settings}, nil
+}
+
+func handleFileCase(file string) (MappingsAndSettings, error) {
+	absPathDirToMappingsAndSettings, _ := filepath.Abs(file)
+
+	_, err := os.Stat(absPathDirToMappingsAndSettings)
+	if err != nil {
+		if errors.Is(err, fs.ErrNotExist) {
+			return MappingsAndSettings{}, fmt.Errorf("the path %v does not exist, %w", file, err)
+		}
+		return MappingsAndSettings{}, fmt.Errorf("stat of the %v returned an  error %w", file, err)
+	}
+
+}
