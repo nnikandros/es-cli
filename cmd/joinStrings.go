e03aafe6af1bccc4516808a9cf6efbad1cc66f0d
Nikitas Nikandros <Nikitas.NIKANDROS@ext.ec.europa.eu>
2026-03-05 15:55:51 +0000

    cleanup

--- cmd/joinStrings.go
+++ cmd/joinStrings.go
@@ -24,23 +24,6 @@ func Reverse[T any](v []T) []T {
 
 }
 
-// // Parse args into a single string. Usually (Always?) args for the es cli are index names. If we provide args then we simply join them. If we don't provide then we read them from  Stdin and  join them with a comma.
-// func ParseArgsIntoString(cmd *cobra.Command, args []string) (index string) {
-// 	indexList := make([]string, 0)
-
-// 	if len(args) > 0 {
-// 		index = strings.Join(args, ",")
-// 	} else {
-// 		scanner := bufio.NewScanner(cmd.InOrStdin())
-// 		for scanner.Scan() {
-// 			indexList = append(indexList, scanner.Text())
-// 		}
-// 		index = strings.Join(indexList, ",")
-// 	}
-
-// 	return
-// }
-
 func ParseArgsIntoString(cmd *cobra.Command, args []string) string {
 	if len(args) > 0 {
 		return strings.Join(args, ",")
